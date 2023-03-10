dec53549dc88df9b0c65961f29f2e57df32bb619
DEVKUMARRAI <91594658+DEVKUMARRAI@users.noreply.github.com>
2023-03-11 00:08:51 +0530

    git ignore added

---
+++ recurcion/quicksort.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int Partition_Function(vector<int>&arr, int low, int high)
+{
+    int i = low;
+    int pivot = arr[low];
+    int j = high;
+    while (i < j)
+    {
+        while (arr[i] <= pivot && i<=high-1)
+        {
+            i++;
+        }
+        while (arr[j] > pivot && j>=low+1)
+        {
+            j--;
+        }
+        if(i<j) swap(arr[i], arr[j]);
+    }
+    swap(arr[low],arr[j]);
+    return j;
+}
+void Quick_Sort(vector<int>&arr, int low, int high)
+{
+    if (low < high)
+    {
+        int partition = Partition_Function(arr, low, high);
+        Quick_Sort(arr, low, partition - 1);
+        Quick_Sort(arr, partition + 1, high);
+    }
+   
+}
+int main()
+{
+    int n;
+    vector<int>arr;
+    cin>>n;
+    while (cin >> n)
+    arr.push_back(n);
+    Quick_Sort(arr, 0, arr.size()-1);
+     cout << "after quick sorting" << endl;
+    for (auto x:arr)
+    {
+        cout<<x<<" ";
+    }
+    
+}
