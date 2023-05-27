ca0c5147d5333582c926342d3cbe27b9b2806ab9
Dev <devrai197551@gmail.com>
2023-05-28 00:04:28 +0530

    Smallest positive missing number completed

--- cpp_practice/smallest_positive_missing_number.cpp
+++ cpp_practice/smallest_positive_missing_number.cpp
@@ -2,7 +2,29 @@
 using namespace std;
 void Smallest_Positive_Missing_Number(int arr[],int n)
 {
-
+    int N=1e6;
+    bool check[N];
+    for(int i=0;i<N;i++)
+    {
+        check[i]=false;
+    }
+    int answer=-1;
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]>=1)
+        {
+            check[arr[i]]=true;
+        }
+    }
+    for(int i=1;i<N;i++)
+    {
+        if(check[i]==false)
+        {
+            answer=i;
+            break;
+        }
+    }
+    cout<<answer<<endl;
 }
 int main()
 {
