23c4ef225734f574206b04d257729467473951c6
Dev <devrai197551@gmail.com>
2023-05-27 23:46:32 +0530

    Smallest positive missing number half complete

---
+++ cpp_practice/smallest_positive_missing_number.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void Smallest_Positive_Missing_Number(int arr[],int n)
+{
+
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    Smallest_Positive_Missing_Number(arr,n);
+}
